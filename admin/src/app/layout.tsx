cb695376edf0a1824ffb89cd1f4389c4e52f602c
iamciscoo <francisjac21@gmail.com>
2025-09-14 20:32:45 +0300

    light theme

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -49,7 +49,7 @@ export default async function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
